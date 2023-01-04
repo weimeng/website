06beaa701590e3889e4fbd32985d837e7fd4093f
Wei-Meng Lee <weimeng@staticfusion.org>
2023-01-04 22:57:42 +0800

    Remove trailing slash

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
+  trailingSlash: `never`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
