f5a70dd15b778dfe5b57e8eed23b258e79d5103f
Wei-Meng Lee <weimeng@staticfusion.org>
2023-03-28 02:21:58 +0800

    Bold section headers

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -8,6 +8,7 @@ interface navigationItem {
 
 function parseNavItems(navItems: any): any {
   return(navItems.map((data: navigationItem, index: number) => {
+    console.log(data)
     return(
       <NavigationItem key={`nav_item_${index}`} navItem={data} />
     )
@@ -25,7 +26,12 @@ const NavigationList = ({ navItems }: { navItems?: navigationItem[] }) => {
 const NavigationItem = ({ navItem }: { navItem: navigationItem }) => {
   return (
     <li>
-      <a href={navItem.path}>{navItem.title}</a>
+      <a
+        href={navItem.path}
+        className={`${ "navigation" in navItem && "font-bold" }`}
+      >
+        {navItem.title}
+      </a>
 
       {"navigation" in navItem &&
         <NavigationList navItems={navItem.navigation} />
