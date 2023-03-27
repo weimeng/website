23e9661527d23e72a802c8e6a2afec047ab1f3d7
Wei-Meng Lee <weimeng@staticfusion.org>
2023-03-28 02:04:52 +0800

    Fix responsive menu btn

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,9 +1,11 @@
-import * as React from "react"
+import React, { useState } from "react"
 import navData from "../data/navigation.yaml"
 
 import NavigationList from "./navigation"
 
 const Sidebar = () => {
+  const [smMenuExpanded, toggleSmMenu] = useState(false);
+
   return (
     <aside className="fixed w-full sm:static sm:table-cell sm:w-72 bg-black text-gray-500 p-3 sm:p-">
       <div className="flex">
@@ -11,12 +13,15 @@ const Sidebar = () => {
           Wei Meng's Website
         </h2>
 
-        <button className="flex-shrink uppercase sm:hidden btn p-0 px-1 js__namespace-menu-btn">
+        <button
+          className="flex-shrink uppercase sm:hidden btn p-0 px-1 js__namespace-menu-btn"
+          onClick={ () => toggleSmMenu(!smMenuExpanded) }
+        >
           Menu
         </button>
       </div>
 
-      <div className="hidden sm:block js__namespace-menu">
+      <div className={`${ smMenuExpanded ? "" : "hidden" } sm:block js__namespace-menu`}>
         <NavigationList navItems={navData.navigation} />
       </div>
     </aside>
